9e3193653c198bd5a131cfcf1abc129104269044
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2023-12-17 19:52:22 +0530

    Integrated starter kit for arcgis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
+import MapView from '@arcgis/core/views/MapView';
+import Map from '@arcgis/core/Map';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const mapRef = useRef(null);
+	useEffect(() => {
+		if (!mapRef?.current) return;
+		const map = new Map({
+			basemap: 'osm',
+		});
+		const view = new MapView({
+			map,
+			container: mapRef.current,
+			center: [55, 25],
+			zoom: 13,
+		});
+		return () => view && view.destroy();
+	}, []);
+	return <div className='viewDiv' ref={mapRef}></div>;
 }
 
 export default App;
